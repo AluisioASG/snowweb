14a7c86cbed97119744ca874fd044f4815ce0b16
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2021-04-18 12:55:34 -0300

    Provide an initial NixOS module
    No so much initial as one that works and that I'll commit while I do
    other stuff.

--- flake.nix
+++ flake.nix
@@ -3,32 +3,158 @@
 # SPDX-License-Identifier: MIT
 
 {
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, flake-utils, nixpkgs }: flake-utils.lib.simpleFlake rec {
-    inherit self nixpkgs;
-    name = "snowweb";
-    overlay = final: prev: {
-      ${name} = {
-        defaultPackage = final.buildGoModule {
-          inherit name;
-          src = self;
-          vendorSha256 = "sha256-6S6OgkSf6xiEHbxRfgDZD/rRlfTn5ryG1evIxNCxwhs=";
-          meta = with nixpkgs.lib; {
-            description = "Static website server for Nix packages";
-            homepage = "https://git.sr.ht/~aasg/snowweb";
-            license = licenses.agpl3;
-            maintainers = with maintainers; [ AluisioASG ];
-            platforms = platforms.linux;
+
+  outputs = { self, flake-utils, nixpkgs }:
+    let
+
+      exports = {
+        overlay = final: prev: {
+          "snowweb" = {
+            defaultPackage = final.buildGoModule {
+              name = "snowweb";
+              src = self;
+              vendorSha256 = "sha256-6S6OgkSf6xiEHbxRfgDZD/rRlfTn5ryG1evIxNCxwhs=";
+              meta = with nixpkgs.lib; {
+                description = "Static website server for Nix packages";
+                homepage = "https://git.sr.ht/~aasg/snowweb";
+                license = licenses.agpl3;
+                maintainers = with maintainers; [ AluisioASG ];
+                platforms = platforms.linux;
+              };
+            };
+
+            devShell = final.mkShell {
+              buildInputs = with final; [ go golangci-lint gopls reuse ];
+            };
           };
         };
 
-        devShell = final.mkShell {
-          buildInputs = with final; [ go golangci-lint gopls reuse ];
-        };
+        nixosModule = { config, lib, pkgs, ... }:
+          let
+            inherit (lib) flip mapAttrs' mkEnableOption mkIf mkOption optionalString types;
+
+            siteOpts = {
+              options = {
+                installable = mkOption {
+                  description = "Package to serve, given as an argument to `nix build`.";
+                  type = types.str;
+                };
+
+                listenAddress = mkOption {
+                  description = ''
+                    Network address at which the web server will listen, given as a colon-separated pair of type and address.
+                  '';
+                  type = types.str;
+                  default = "tcp:127.0.0.1:0";
+                };
+
+                certFile = mkOption {
+                  description = "Path to the PEM-encoded certificate used for HTTPS.";
+                  type = types.nullOr types.path;
+                  default = null;
+                };
+
+                keyFile = mkOption {
+                  description = "Path to the PEM-encoded key used for HTTPS.";
+                  type = types.nullOr types.path;
+                  default = null;
+                };
+
+                clientCaFile = mkOption {
+                  description = ''
+                    Path to the PEM-encoded certificate bundle used for client verification.
+                    This is required to enable the remote control API.
+                  '';
+                  type = types.nullOr types.path;
+                  default = null;
+                };
+              };
+            };
+
+            cfg = config.services.snowweb;
+          in
+          {
+            options.services.snowweb = {
+              enable = mkEnableOption "the SnowWeb static web server";
+
+              package = mkOption {
+                description = "snowweb package to use.";
+                type = types.package;
+                default = pkgs.snowweb.defaultPackage;
+                defaultText = "pkgs.snowweb.defaultPackage";
+              };
+
+              nixPackage = mkOption {
+                description = "nix package used to build sites.";
+                type = types.package;
+                default = config.nix.package;
+                defaultText = "config.nix.package";
+              };
+
+              sites = mkOption {
+                description = "Sites to serve through SnowWeb.";
+                type = types.attrsOf (types.submodule siteOpts);
+                default = { };
+              };
+            };
+
+            config = mkIf cfg.enable {
+              systemd.services = flip mapAttrs' cfg.sites (siteName: siteCfg: {
+                name = "snowweb@${siteName}";
+                value = {
+                  description = "SnowWeb web server for ${siteName}";
+                  requires = [ "network.target" ];
+                  after = [ "network.target" ];
+                  wantedBy = [ "multi-user.target" ];
+                  environment.NIX_REMOTE = "daemon";
+                  environment.XDG_CACHE_HOME = "/tmp";
+                  path = [ cfg.nixPackage pkgs.gitMinimal ];
+                  serviceConfig = {
+                    Type = "exec";
+                    ExecStart = ''
+                      ${cfg.package}/bin/snowweb \
+                      ${optionalString (siteCfg.certFile != null) "-certificate ${siteCfg.certFile}"} \
+                      ${optionalString (siteCfg.keyFile != null) "-key ${siteCfg.keyFile}"} \
+                      ${optionalString (siteCfg.clientCaFile != null) "-client-ca ${siteCfg.clientCaFile}"} \
+                      -listen ${siteCfg.listenAddress} \
+                      -log journald \
+                      ${siteCfg.installable}
+                    '';
+                    ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+                    Restart = "on-failure";
+
+                    DynamicUser = true;
+                    NoNewPrivileges = true;
+                    ProtectSystem = "strict";
+                    ProtectHome = true;
+                    ProtectKernelLogs = true;
+                    ProtectKernelModules = true;
+                    ProtectKernelTunables = true;
+                    ProtectControlGroups = true;
+                    PrivateDevices = true;
+                    PrivateTmp = true;
+                    DevicePolicy = "closed";
+                    MemoryDenyWriteExecute = true;
+                  };
+                };
+              });
+            };
+          };
       };
-    };
-  };
+
+      outputs = flake-utils.lib.simpleFlake rec {
+        inherit self nixpkgs;
+        name = "snowweb";
+        inherit (exports) overlay;
+      };
+
+    in
+
+    exports // outputs;
+
 }
